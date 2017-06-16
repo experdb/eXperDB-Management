aa5c76755e0363842951dc00aa57e750f2909d61
thpark-PC\thpark <parks007net@gmail.com>
2017-06-16 17:33:00 +0900

    수정

--- DX-TControlAgent/bin/status.sh
+++ DX-TControlAgent/bin/status.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
-echo "DX-TcontrolAgent status.. "
 
-ps -ef | grep java | grep -v 'grep'| grep Du=DX-TcontrolAgent
+
+PGM_NAME=DX-TcontrolAgent
+DATE=`date +%Y%m%d-%H%M%S`
+ 
+Cnt=`ps -ef|grep $PGM_NAME|grep -v grep|grep -v vi|wc -l`
+PROCESS=`ps -ef|grep $PGM_NAME|grep -v grep|grep -v vi|awk '{print $2}'`
+ 
+
+echo "############################################################################################# "
+echo "##################                                                         ################## "
+echo "################## Program : $PGM_NAME                              ################## "
+
+if [ $Cnt -gt 0 ]
+then
+   echo "################## $DATE                                         ##################" 
+   echo "################## status : running                                        ##################" 
+   echo "################## PID    : $PROCESS                                          ##################"
+else
+   echo "################## $DATE                                         ##################" 
+   echo "################## status : stop                                           ##################"
+   echo "################## PID    :                                                ##################"
+fi
+echo "##################                                                         ################## "
+echo "############################################################################################# "
+
+
