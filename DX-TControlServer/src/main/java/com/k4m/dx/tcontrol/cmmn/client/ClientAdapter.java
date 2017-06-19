534aa466da1c618d89d9d74f9694c4b2ca189c03
thpark-PC\thpark <parks007net@gmail.com>
2017-06-19 11:37:58 +0900

    Client 통신

---
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/cmmn/client/ClientAdapter.java
@@ -0,0 +1,155 @@
+package com.k4m.dx.tcontrol.cmmn.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+public class ClientAdapter {
+	private final ClientConnector cc;
+	private final String IP;
+	private final int PORT;
+	public ClientAdapter(String IP, int PORT){
+		cc = new ClientConnector();
+		this.IP = IP;
+		this.PORT = PORT;
+	}
+	
+	/**
+	 * socket Open
+	 * @throws Exception
+	 */
+	public void open() throws Exception{
+		cc.create(IP, PORT);
+	}
+	
+	/**
+	 * socket close
+	 * @throws Exception
+	 */
+	public void close() throws Exception{
+
+		JSONObject jObj = new JSONObject();
+
+		jObj.put(ClientProtocolID.DX_EX_CODE, ClientTranCodeType.CLOSE);
+		
+		byte[] bt = jObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		cc.close();
+	}
+	
+	private List<String> SubReqMsg(byte[] recvBuff) throws Exception{
+		JSONParser parser=new JSONParser();
+		JSONObject obj=(JSONObject)parser.parse(new String(recvBuff));
+		JSONArray jArray=(JSONArray) obj.get(ClientProtocolID.RESULT_DATA);
+		String _tran_err_msg = (String)obj.get(ClientProtocolID.ERR_MSG);
+		
+		if (_tran_err_msg == null){
+			List<String> _tran_req_data = new ArrayList<String>();
+			if (jArray != null){		
+				for(Object map: jArray){
+					_tran_req_data.add((String)map);
+				}
+			}
+			
+			return _tran_req_data;
+		}else{
+			throw new Exception(_tran_err_msg);
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param recvBuff
+	 * @return
+	 * @throws Exception
+	 */
+	private JSONObject parseToJsonObj(byte[] recvBuff) throws Exception{
+		JSONParser parser=new JSONParser();
+		JSONObject obj=(JSONObject)parser.parse(new String(recvBuff));
+		return obj;
+	}
+	
+	/**
+	 * DB List 조회
+	 * @param tran_cd
+	 * @param serverObj
+	 * @return
+	 * @throws Exception
+	 */
+	public JSONObject dxT001(String strDxExCode, JSONObject serverObj) throws Exception{
+		JSONObject jObj = new JSONObject();
+		jObj.put(ClientProtocolID.DX_EX_CODE, strDxExCode);
+		jObj.put(ClientProtocolID.SERVER_INFO, serverObj);
+
+		byte[] bt = jObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		byte[]	recvBuff = cc.recv(4, false);
+		return parseToJsonObj(recvBuff);
+	}
+	
+	
+	/**
+	 * table List 조회
+	 * @param tran_cd
+	 * @param serverObj
+	 * @return
+	 * @throws Exception
+	 */
+	public JSONObject dxT002(String strDxExCode, JSONObject serverObj, String strSchema) throws Exception{
+		JSONObject jObj = new JSONObject();
+		jObj.put(ClientProtocolID.DX_EX_CODE, strDxExCode);
+		jObj.put(ClientProtocolID.SERVER_INFO, serverObj);
+		jObj.put(ClientProtocolID.SCHEMA, strSchema);
+
+		byte[] bt = jObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		byte[]	recvBuff = cc.recv(4, false);
+		return parseToJsonObj(recvBuff);
+	}
+	
+	public JSONObject dxT003(String strDxExCode, JSONObject serverObj) throws Exception{
+		JSONObject jObj = new JSONObject();
+		jObj.put(ClientProtocolID.DX_EX_CODE, strDxExCode);
+		jObj.put(ClientProtocolID.SERVER_INFO, serverObj);
+
+
+		byte[] bt = jObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		byte[]	recvBuff = cc.recv(4, false);
+		return parseToJsonObj(recvBuff);
+	}
+	
+	public JSONObject dxT005(JSONObject reqJObj) throws Exception{
+
+
+		byte[] bt = reqJObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		byte[]	recvBuff = cc.recv(4, false);
+		return parseToJsonObj(recvBuff);
+	}
+	
+	public JSONObject dxT006(String strDxExCode, JSONObject jObj) throws Exception{
+
+		byte[] bt = jObj.toString().getBytes();
+		
+		cc.send(4, bt);
+		
+		byte[]	recvBuff = cc.recv(4, false);
+		return parseToJsonObj(recvBuff);
+	}
+}
---
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/cmmn/client/ClientTranCodeType.java
@@ -0,0 +1,31 @@
+package com.k4m.dx.tcontrol.cmmn.client;
+
+public class ClientTranCodeType {
+	public static final String DxT001 = "DxT001";
+	public static final String DxT002 = "DxT002";
+	public static final String DxT003 = "DxT003";
+	public static final String DxT004 = "DxT004";
+	public static final String DxT005 = "DxT005";
+	public static final String DxT006 = "DxT006";
+	public static final String DxT007 = "DxT007";
+	public static final String DxT008 = "DxT008";
+	
+	public static final String STATUS = "STATUS";
+	public static final String STOP = "STOP";
+	public static final String CLOSE = "CLOSE";
+
+	
+	/**
+	 * 결과
+	 */
+	public static final String RESULT = "RESULT";								  //결과
+	
+
+	/**
+	 * DB커넥션 테스트
+	 */
+	public static final String WEB_DB_CONN_TEST = "WEB_DB_CONN_TEST";			  //DB커넥션 테스트
+	
+
+	
+}
