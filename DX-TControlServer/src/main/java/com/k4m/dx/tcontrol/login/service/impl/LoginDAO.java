f0e76356aec78a9f5244a066143caaa286712a5c
kimjy <kimjy@k4m.com>
2017-06-19 16:54:26 +0900

    2017.06.19

--- DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/login/service/impl/LoginDAO.java
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/login/service/impl/LoginDAO.java
@@ -3,11 +3,8 @@ package com.k4m.dx.tcontrol.login.service.impl;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import com.ibatis.sqlmap.client.SqlMapClient;
 import com.k4m.dx.tcontrol.login.service.UserVO;
 
 import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
