037f729e1b31373ae962f0265a39169797e727cc
kimjy <kimjy@k4m.com>
2017-07-07 13:37:10 +0900

    2017.07.07

--- DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/cmmn/EgovImgPaginationRenderer.java
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/cmmn/EgovImgPaginationRenderer.java
@@ -52,12 +52,12 @@ public class EgovImgPaginationRenderer extends AbstractPaginationRenderer implem
 	*/
 	public void initVariables() {
 
-		firstPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_pre10.gif' border=0/></a>&#160;";
-		previousPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_pre1.gif' border=0/></a>&#160;";
-		currentPageLabel = "<strong>{0}</strong>&#160;";
-		otherPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">{2}</a>&#160;";
-		nextPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_next1.gif' border=0/></a>&#160;";
-		lastPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_next10.gif' border=0/></a>&#160;";
+		firstPageLabel = "<li><a href=\"#\" onclick=\"{0}({1}); return false;\"><<</a><li>&#160;";
+		previousPageLabel = "<li><a href=\"#\" onclick=\"{0}({1}); return false;\"><<li></a>&#160;";
+		currentPageLabel = "<li><span class='curr'>{0}</span><li>&#160;";
+		otherPageLabel = "<li><a href=\"#\" onclick=\"{0}({1}); return false;\">{2}</a><li>&#160;";
+		nextPageLabel = "<li><a href=\"#\" onclick=\"{0}({1}); return false;\">></a><li>&#160;";
+		lastPageLabel = "<li><a href=\"#\" onclick=\"{0}({1}); return false;\">>></a><li>&#160;";
 	}
 
 	@Override
