9e08aa2930496f361707c0ec5dfb0969e4826f72
kimjy <kimjy@k4m.com>
2017-07-21 13:52:30 +0900

    2017.07.21

--- DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/accesscontrol/service/AccessControlService.java
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/accesscontrol/service/AccessControlService.java
@@ -3,9 +3,10 @@ package com.k4m.dx.tcontrol.accesscontrol.service;
 import java.util.List;
 
 public interface AccessControlService {
-	
+
 	/**
 	 * DB 조회
+	 * 
 	 * @param db_svr_id
 	 * @return
 	 * @throws Exception
@@ -14,26 +15,37 @@ public interface AccessControlService {
 
 	/**
 	 * DB,SERVER 조회
+	 * 
 	 * @param db_id
 	 * @return
 	 * @throws Exception
 	 */
-	List<DbIDbServerVO> selectServerDb(int db_id)throws Exception;
+	List<DbIDbServerVO> selectServerDb(int db_id) throws Exception;
 
 	/**
-	 * DB접근제어 삭제
+	 * DB SERVER Name 조회
+	 * 
 	 * @param db_svr_id
 	 * @return
 	 * @throws Exception
 	 */
-	void deleteDbAccessControl(int db_svr_id)throws Exception;
-	
+	List<DbIDbServerVO> selectDbServerName(int db_svr_id) throws Exception;
+
+	/**
+	 * DB접근제어 삭제
+	 * 
+	 * @param db_svr_id
+	 * @return
+	 * @throws Exception
+	 */
+	void deleteDbAccessControl(int db_svr_id) throws Exception;
+
 	/**
 	 * 접근제어 등록
+	 * 
 	 * @param accessControlVO
 	 * @throws Exception
 	 */
-	void insertAccessControl(AccessControlVO accessControlVO)throws Exception;
-	
-	
+	void insertAccessControl(AccessControlVO accessControlVO) throws Exception;
+
 }
--- DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/accesscontrol/service/impl/AccessControlDAO.java
+++ DX-TControlServer/src/main/java/com/k4m/dx/tcontrol/accesscontrol/service/impl/AccessControlDAO.java
@@ -11,28 +11,36 @@ import com.k4m.dx.tcontrol.accesscontrol.service.DbIDbServerVO;
 import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
 
 @Repository("accessControlDAO")
-public class AccessControlDAO extends EgovAbstractMapper{
+public class AccessControlDAO extends EgovAbstractMapper {
 
 	@SuppressWarnings({ "deprecation", "unchecked" })
-	public List<DbIDbServerVO> selectDatabaseList(int db_svr_id) throws SQLException{
+	public List<DbIDbServerVO> selectDatabaseList(int db_svr_id) throws SQLException {
 		List<DbIDbServerVO> result = null;
-		result = (List<DbIDbServerVO>) list("accessControlSql.selectDatabaseList", db_svr_id);	
+		result = (List<DbIDbServerVO>) list("accessControlSql.selectDatabaseList", db_svr_id);
 		return result;
 	}
 
 	@SuppressWarnings({ "unchecked", "deprecation" })
 	public List<DbIDbServerVO> selectServerDb(int db_id) throws SQLException {
 		List<DbIDbServerVO> result = null;
-		result = (List<DbIDbServerVO>) list("accessControlSql.selectServerDb", db_id);	
+		result = (List<DbIDbServerVO>) list("accessControlSql.selectServerDb", db_id);
 		return result;
 	}
 
-	public void deleteDbAccessControl(int db_svr_id) throws SQLException{
-		delete("accessControlSql.deleteDbAccessControl",db_svr_id);
-		
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public List<DbIDbServerVO> selectDbServerName(int db_svr_id) {
+		List<DbIDbServerVO> result = null;
+		result = (List<DbIDbServerVO>) list("accessControlSql.selectDbServerName", db_svr_id);
+		return result;
 	}
-	public void insertAccessControl(AccessControlVO accessControlVO) throws SQLException{
-		insert("accessControlSql.insertAccessControl", accessControlVO);		
+
+	public void deleteDbAccessControl(int db_svr_id) throws SQLException {
+		delete("accessControlSql.deleteDbAccessControl", db_svr_id);
+
+	}
+
+	public void insertAccessControl(AccessControlVO accessControlVO) throws SQLException {
+		insert("accessControlSql.insertAccessControl", accessControlVO);
 	}
 
 }
