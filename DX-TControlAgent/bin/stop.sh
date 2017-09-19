057f86adf99d7cf52d744ce788a4d2330d2eb390
thpark-PC\thpark <parks007net@gmail.com>
2017-09-19 14:23:58 +0900

    09/19

--- DX-TControlAgent/bin/stop.sh
+++ DX-TControlAgent/bin/stop.sh
@@ -7,7 +7,7 @@ JAVA_HOME=/home/devel/DX-TcontrolAgent/java/jre1.7.0_80
 PROJECT_HOME=/home/devel
 CURRENT_DIR=$PROJECT_HOME/DX-TcontrolAgent
 
-LOG_DIR=$CURRENT_DIR/log
+LOG_DIR=$CURRENT_DIR/logs
 
 MAIN_CLASS=com.k4m.dx.tcontrol.DaemonStart
 
@@ -20,5 +20,5 @@ LIB=$CURRENT_DIR/lib/*
 JAVA_CLASSPATH=$APP_HOME:$LIB
 
 
-$JAVA_HOME/bin/java -Du=DX-TcontrolAgent -Dlog.base=$LOG_DIR/DX-TcontrolAgent -classpath $JAVA_CLASSPATH $MAIN_CLASS -shutdown
+$JAVA_HOME/bin/java -Du=DX-TcontrolAgent -Dlog.base=$LOG_DIR -classpath $JAVA_CLASSPATH $MAIN_CLASS -shutdown
 
