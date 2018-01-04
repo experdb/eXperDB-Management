ff2ee02b7b7fa86fe6de67353eb0387468559754
swbyun <swbyun@k4m.com>
2018-01-04 15:05:43 +0900

    encript 수정

---
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encript/agentmonitoring/web/EncriptAgentMonitoringController.java
@@ -0,0 +1,57 @@
+package com.k4m.dx.tcontrol.encript.agentmonitoring.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.k4m.dx.tcontrol.admin.agentmonitoring.service.AgentMonitoringVO;
+import com.k4m.dx.tcontrol.cmmn.CmmnUtils;
+import com.k4m.dx.tcontrol.common.service.HistoryVO;
+
+/**
+ * EncriptAgent 모니터링 컨트롤러 클래스를 정의한다.
+ *
+ * @author 변승우
+ * @see
+ * 
+ *      <pre>
+ * == 개정이력(Modification Information) ==
+ *
+ *   수정일       수정자           수정내용
+ *  -------     --------    ---------------------------
+ *  2018.01.04   변승우 최초 생성
+ *      </pre>
+ */
+
+
+@Controller
+public class EncriptAgentMonitoringController {
+
+	
+	/**
+	 * EncriptAgent 모니터링 화면을 보여준다.
+	 * 
+	 * @param historyVO
+	 * @param request
+	 * @return ModelAndView mv
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/encriptAgentMonitoring.do")
+	public ModelAndView encriptAgentMonitoring(HttpServletRequest request, ModelMap model) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			mv.setViewName("encript/agentMonitoring/agentMonitoring");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mv;
+
+	}
+	
+}
