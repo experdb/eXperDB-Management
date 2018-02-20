56f7b489721f4e77fad138f38134c0d8e5e3ffba
swbyun <swbyun@k4m.com>
2018-02-20 18:13:53 +0900

    수정

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encript/setting/web/EncriptSettingController.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encript/setting/web/EncriptSettingController.java
@@ -2,10 +2,16 @@ package com.k4m.dx.tcontrol.encript.setting.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.k4m.dx.tcontrol.encript.service.call.EncriptSettingServiceCall;
+import com.k4m.dx.tcontrol.encript.service.call.KeyManageServiceCall;
 
 /**
  * Encript 설정 컨트롤러 클래스를 정의한다.
@@ -25,6 +31,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EncriptSettingController {
+	
+	String restIp = "127.0.0.1";
+	int restPort = 8443;
+	String strTocken = "uq1b/dgOIzpzH+EAD9UOl5Iz26soa1H+hdbmD38noqs=";
 
 	/**
 	 * Encript 보안정책 옵션 설정 화면을 보여준다.
@@ -37,12 +47,52 @@ public class EncriptSettingController {
 	@RequestMapping(value = "/securityPolicyOptionSet.do")
 	public ModelAndView encriptAgentMonitoring(HttpServletRequest request, ModelMap model) {
 		ModelAndView mv = new ModelAndView();
-		try {
+		try {		
 			mv.setViewName("encript/setting/securityPolicyOptionSet");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return mv;
+	}
+	
+	
+	/**
+	 * 보안정책 옵션 설정 조회1
+	 * 
+	 * @return resultSet
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/selectSysConfigListLike.do")
+	public @ResponseBody JSONArray selectCryptoKeyList(HttpServletRequest request) {
+			
+		EncriptSettingServiceCall essc = new EncriptSettingServiceCall();
+		JSONArray result = new JSONArray();
+		try {
+			result = essc.selectSysConfigListLike(restIp, restPort, strTocken);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+
+	/**
+	 * 보안정책 옵션 설정 조회2
+	 * 
+	 * @return resultSet
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/selectSysMultiValueConfigListLike.do")
+	public @ResponseBody JSONArray selectSysMultiValueConfigListLike(HttpServletRequest request) {
+			
+		EncriptSettingServiceCall essc = new EncriptSettingServiceCall();
+		JSONArray result = new JSONArray();
+		try {
+			result = essc.selectSysMultiValueConfigListLike(restIp, restPort, strTocken);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	
