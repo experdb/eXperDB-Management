9dfda54e81278bbd110c652e60873c8a9da1cce6
swbyun <swbyun@k4m.com>
2018-03-13 14:24:48 +0900

    수정

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encrypt/service/call/EncryptSettingServiceCall.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encrypt/service/call/EncryptSettingServiceCall.java
@@ -1,5 +1,6 @@
 package com.k4m.dx.tcontrol.encrypt.service.call;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +19,7 @@ import com.k4m.dx.tcontrol.cmmn.serviceproxy.EncryptCommonService;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.SystemCode;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.TypeUtility;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.AdminServerPasswordRequest;
+import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.Entity;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.MasterKeyFile;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SysConfig;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SysMultiValueConfig;
@@ -665,5 +667,87 @@ public class EncryptSettingServiceCall {
 		}
 		return resultJson;
 
+	}
+
+	public JSONArray selectAgentMonitoring(String restIp, int restPort, String strTocken, String loginId,
+			String entityId) throws Exception {
+		
+		JSONArray jsonArray = new JSONArray();
+		
+		EncryptCommonService api = new EncryptCommonService(restIp, restPort);
+
+		String strService = SystemCode.ServiceName.ENTITY_SERVICE;
+		String strCommand = SystemCode.ServiceCommand.SELECTENTITYLIST;
+
+		String monitored = "false"; 
+		
+		Entity param = new Entity();
+		param.setEntityTypeCode(SystemCode.EntityTypeCode.AGENT);
+
+		HashMap body = new HashMap();
+		//body.put(TypeUtility.getJustClassName(param.getClass()), param.toJSONString());
+		body.put("monitored", monitored);
+		body.put(TypeUtility.getJustClassName(param.getClass()), param.toJSONString());
+		
+		String parameters = TypeUtility.makeRequestBody(body);
+		
+		System.out.println(parameters);
+
+		HashMap header = new HashMap();
+		header.put(SystemCode.FieldName.LOGIN_ID, loginId);
+		header.put(SystemCode.FieldName.ENTITY_UID, entityId);
+		header.put(SystemCode.FieldName.TOKEN_VALUE, strTocken);
+
+		JSONObject resultJson = api.callService(strService, strCommand, header, parameters.toString());
+		
+		Iterator<?> iter = resultJson.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+
+			System.out.println(String.valueOf(entry.getKey()) + " = " + String.valueOf(entry.getValue()));
+		}
+		
+		
+		String resultCode = (String) resultJson.get("resultCode");
+		String resultMessage = (String) resultJson.get("resultMessage");
+		//long totalListCount = (long) resultJson.get("totalListCount");
+		
+		
+		if(resultCode.equals("0000000000")) {
+			ArrayList list = (ArrayList) resultJson.get("list");
+			
+			//System.out.println("list Size : " + list.size());
+			//if(totalListCount > 0) {
+				for(int i=0; i<list.size(); i++) {
+					JSONObject data = (JSONObject) list.get(i);
+					JSONObject jsonObj = new JSONObject();
+					Entity entity = new Entity();
+					Gson gson = new Gson();
+					entity = gson.fromJson(data.toJSONString(), entity.getClass());
+					
+					jsonObj.put("no", i+1);
+					jsonObj.put("createDateTime", entity.getCreateDateTime());
+					jsonObj.put("receivedPolicyVersion", entity.getReceivedPolicyVersion());
+					jsonObj.put("entityUid", entity.getEntityUid());
+					jsonObj.put("entityTypeCode", entity.getEntityTypeCode());
+					jsonObj.put("latestDateTime", entity.getLatestDateTime());
+					jsonObj.put("sentPolicyVersion", entity.getSentPolicyVersion());
+					jsonObj.put("latestAddress", entity.getLatestAddress());
+					jsonObj.put("entityStatusName", entity.getEntityStatusName());
+					jsonObj.put("entityName", new String(entity.getEntityName().toString().getBytes("iso-8859-1"),"UTF-8"));
+					jsonObj.put("updateName", new String(entity.getUpdateName().toString().getBytes("iso-8859-1"),"UTF-8"));
+					jsonObj.put("appVersion", entity.getAppVersion());
+					jsonObj.put("entityStatusCode", entity.getEntityStatusCode());
+					jsonObj.put("updateDateTime", entity.getUpdateDateTime());
+					jsonObj.put("resultMessage", resultCode);
+					jsonObj.put("resultCode", resultMessage);
+
+					jsonArray.add(jsonObj);
+
+					System.out.println("getEntityUid : " + entity.getEntityUid());
+					System.out.println("getEntityName : " + new String(entity.getEntityName().toString().getBytes("iso-8859-1"),"UTF-8") );
+			}
+		}
+		return jsonArray;
 	}
 }
