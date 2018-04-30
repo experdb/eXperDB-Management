dcb62898005bb26cd1434e95db1072178d085958
thpark-PC\thpark <parks007net@gmail.com>
2018-04-30 17:22:57 +0900

    Query Encryption Agent Status

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/serviceproxy/ServiceCallTest.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/serviceproxy/ServiceCallTest.java
@@ -33,6 +33,7 @@ import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.ProfileProtection;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SysCode;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SysConfig;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SysMultiValueConfig;
+import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SystemStatus;
 import com.k4m.dx.tcontrol.cmmn.serviceproxy.vo.SystemUsage;
 
 
@@ -195,7 +196,10 @@ public class ServiceCallTest {
 		//test.selectEntityUid(restIp, restPort, strTocken, loginId, entityId, loginId);
 		
 		//암호화 통계
-		test.selectAuditLogSiteHourForStat(restIp, restPort, strTocken, loginId, entityId);
+		//test.selectAuditLogSiteHourForStat(restIp, restPort, strTocken, loginId, entityId);
+		
+		//암호화 Agent상태조회
+		test.selectSystemStatus(restIp, restPort, strTocken, loginId, entityId);
 		
 	}
 
@@ -2837,6 +2841,92 @@ public class ServiceCallTest {
 		}
 
 	}
+	
+	
+	private void selectSystemStatus(String restIp, int restPort, String strTocken, String loginId, String entityId) throws Exception {
+
+		EncryptCommonService api = new EncryptCommonService(restIp, restPort);
+
+		String strService = SystemCode.ServiceName.ENTITY_SERVICE;
+		String strCommand = SystemCode.ServiceCommand.SELECTSYSTEMSTATUS;
+
+		SystemStatus param = new SystemStatus();
+
+		param.setMonitoredUid(""); //전체
+
+
+		//JSONObject parameters = new JSONObject();
+		//parameters.put("profile", vo);
+		
+		HashMap body = new HashMap();
+		body.put(TypeUtility.getJustClassName(param.getClass()), param.toJSONString());
+
+		String parameters = TypeUtility.makeRequestBody(body);
+
+		HashMap header = new HashMap();
+		//header.put(SystemCode.FieldName.LOGIN_ID, loginId);
+		//header.put(SystemCode.FieldName.ENTITY_UID, entityId);
+		header.put(SystemCode.FieldName.LOGIN_ID, loginId);
+		header.put(SystemCode.FieldName.ENTITY_UID, entityId);
+		header.put(SystemCode.FieldName.TOKEN_VALUE, strTocken);
+
+		JSONObject resultJson = api.callService(strService, strCommand, header, parameters);
+		
+
+		String resultCode = (String) resultJson.get("resultCode");
+		String resultMessage = (String) resultJson.get("resultMessage");
+		resultMessage = new String(resultMessage.getBytes("iso-8859-1"),"UTF-8"); 
+		
+		System.out.println("resultCode : " + resultCode + " resultMessage : " + resultMessage);
+		
+		if(resultCode.equals(SystemCode.ResultCode.SUCCESS)) {
+			
+			ArrayList list = (ArrayList) resultJson.get("list");
+			if(list != null) {
+				for (int j = 0; j < list.size(); j++) {
+					JSONObject jsonObj = (JSONObject) list.get(j);
+					
+					
+					Gson gson = new Gson();
+					SystemStatus systemStatus = new SystemStatus();
+					systemStatus = gson.fromJson(jsonObj.toJSONString(), systemStatus.getClass());
+					
+					String serverLogDateTime = systemStatus.getServerLogDateTime();
+					String siteLogDateTime = systemStatus.getSiteLogDateTime();
+					String monitoredAddress = systemStatus.getMonitoredAddress();
+					String siteAccessAddress = systemStatus.getSiteAccessAddress();
+					String monitoredUid = systemStatus.getMonitoredUid();
+					String monitoredName = systemStatus.getMonitoredName();
+					String targetType = systemStatus.getTargetType();
+					String targetUid = systemStatus.getTargetUid();
+					String targetName = systemStatus.getTargetName();
+					String monitorType = systemStatus.getMonitorType();
+					String resultLevel = systemStatus.getResultLevel();
+					String logMessage = systemStatus.getLogMessage();
+					
+
+					System.out.println("serverLogDateTime : " + serverLogDateTime);
+					System.out.println("siteLogDateTime : " + siteLogDateTime);
+					System.out.println("monitoredAddress : " + monitoredAddress);
+					System.out.println("siteAccessAddress : " + siteAccessAddress);
+					System.out.println("monitoredUid : " + monitoredUid);
+					System.out.println("monitoredName : " + monitoredName);
+					System.out.println("targetType : " + targetType);
+					System.out.println("targetUid : " + targetUid );
+					System.out.println("targetName : " + targetName );
+					System.out.println("monitorType : " + monitorType );
+					System.out.println("resultLevel : " + resultLevel );
+					System.out.println("logMessage : " + logMessage );
+				}
+			}
+			
+		} else {
+			
+		}
+
+	}
+	
+	
 	private void checkChar(String originalStr) throws Exception {
 		//String originalStr = "Å×½ºÆ®"; // 테스트 
 		String [] charSet = {"utf-8","euc-kr","ksc5601","iso-8859-1","x-windows-949"};
