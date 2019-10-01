8f3a725ab59232c477f1f6dc1e0a7a457ee532e1
joo01224 <kimjy@k4m.com>
2019-10-01 14:18:20 +0900

    type : feat body : db2pg setting feat

---
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/setting/service/CodeVO.java
@@ -0,0 +1,30 @@
+package com.k4m.dx.tcontrol.db2pg.setting.service;
+
+public class CodeVO {
+	private String grp_cd;
+	private String sys_cd;
+	private String sys_cd_nm;
+	
+	public String getGrp_cd() {
+		return grp_cd;
+	}
+	public void setGrp_cd(String grp_cd) {
+		this.grp_cd = grp_cd;
+	}
+	public String getSys_cd() {
+		return sys_cd;
+	}
+	public void setSys_cd(String sys_cd) {
+		this.sys_cd = sys_cd;
+	}
+	public String getSys_cd_nm() {
+		return sys_cd_nm;
+	}
+	public void setSys_cd_nm(String sys_cd_nm) {
+		this.sys_cd_nm = sys_cd_nm;
+	}
+
+	
+
+
+}
