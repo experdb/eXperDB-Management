1372fd71a94b5b75353d7388b2809ec6e46fa6c6
swbyu <swbyu@seungwoo-pc>
2019-10-07 10:37:31 +0900

    type : feat body : new add Db2pgSysInfVO

---
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/dbms/service/Db2pgSysInfVO.java
@@ -0,0 +1,132 @@
+package com.k4m.dx.tcontrol.db2pg.dbms.service;
+
+public class Db2pgSysInfVO {
+
+	private int db2pg_sys_id;
+	private String db2pg_sys_nm;
+	private String dbms_dscd;
+	private String ipadr;
+	private String dtb_nm;
+	private String spr_usr_id;
+	private int portno;
+	private String scm_nm;
+	private String pwd;
+	private String crts_nm;
+	private String frst_regr_id;
+	private String frst_reg_dtm;
+	private String lst_mdfr_id;
+	private String lst_mdf_dtm;
+
+	public int getDb2pg_sys_id() {
+		return db2pg_sys_id;
+	}
+
+	public void setDb2pg_sys_id(int db2pg_sys_id) {
+		this.db2pg_sys_id = db2pg_sys_id;
+	}
+
+	public String getDb2pg_sys_nm() {
+		return db2pg_sys_nm;
+	}
+
+	public void setDb2pg_sys_nm(String db2pg_sys_nm) {
+		this.db2pg_sys_nm = db2pg_sys_nm;
+	}
+
+	public String getDbms_dscd() {
+		return dbms_dscd;
+	}
+
+	public void setDbms_dscd(String dbms_dscd) {
+		this.dbms_dscd = dbms_dscd;
+	}
+
+	public String getIpadr() {
+		return ipadr;
+	}
+
+	public void setIpadr(String ipadr) {
+		this.ipadr = ipadr;
+	}
+
+	public String getDtb_nm() {
+		return dtb_nm;
+	}
+
+	public void setDtb_nm(String dtb_nm) {
+		this.dtb_nm = dtb_nm;
+	}
+
+	public String getSpr_usr_id() {
+		return spr_usr_id;
+	}
+
+	public void setSpr_usr_id(String spr_usr_id) {
+		this.spr_usr_id = spr_usr_id;
+	}
+
+	public int getPortno() {
+		return portno;
+	}
+
+	public void setPortno(int portno) {
+		this.portno = portno;
+	}
+
+	public String getScm_nm() {
+		return scm_nm;
+	}
+
+	public void setScm_nm(String scm_nm) {
+		this.scm_nm = scm_nm;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getCrts_nm() {
+		return crts_nm;
+	}
+
+	public void setCrts_nm(String crts_nm) {
+		this.crts_nm = crts_nm;
+	}
+
+	public String getFrst_regr_id() {
+		return frst_regr_id;
+	}
+
+	public void setFrst_regr_id(String frst_regr_id) {
+		this.frst_regr_id = frst_regr_id;
+	}
+
+	public String getFrst_reg_dtm() {
+		return frst_reg_dtm;
+	}
+
+	public void setFrst_reg_dtm(String frst_reg_dtm) {
+		this.frst_reg_dtm = frst_reg_dtm;
+	}
+
+	public String getLst_mdfr_id() {
+		return lst_mdfr_id;
+	}
+
+	public void setLst_mdfr_id(String lst_mdfr_id) {
+		this.lst_mdfr_id = lst_mdfr_id;
+	}
+
+	public String getLst_mdf_dtm() {
+		return lst_mdf_dtm;
+	}
+
+	public void setLst_mdf_dtm(String lst_mdf_dtm) {
+		this.lst_mdf_dtm = lst_mdf_dtm;
+	}
+
+}
