e81b41314a1ee48c67ed93bee7a45f0c1050e9a7
swbyu <swbyu@seungwoo-pc>
2019-10-08 11:40:01 +0900

    type : feat body : Client Information for AGENT Use

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/client/ClientTranCodeType.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/client/ClientTranCodeType.java
@@ -46,6 +46,7 @@ public class ClientTranCodeType {
 	public static final String DxT030 = "DxT030";
 	public static final String DxT031 = "DxT031";
 	public static final String DxT032 = "DxT032";
+	public static final String DxT033 = "DxT033";
 	
 	
 	public static final String STATUS = "STATUS";
@@ -63,6 +64,7 @@ public class ClientTranCodeType {
 	 * DB커넥션 테스트
 	 */
 	public static final String WEB_DB_CONN_TEST = "WEB_DB_CONN_TEST";			  //DB커넥션 테스트
+
 	
 
 	
