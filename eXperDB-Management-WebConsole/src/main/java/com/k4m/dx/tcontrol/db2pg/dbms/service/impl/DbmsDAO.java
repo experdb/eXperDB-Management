ae49ee6037167e9cf358fdb97dea3a8a71f67c77
swbyu <swbyu@seungwoo-pc>
2019-10-08 11:39:13 +0900

    type : feat body : DB2PG DBML list screen development and inquiry function

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/dbms/service/impl/DbmsDAO.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/dbms/service/impl/DbmsDAO.java
@@ -1,19 +1,52 @@
 package com.k4m.dx.tcontrol.db2pg.dbms.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Repository;
+
+import com.k4m.dx.tcontrol.admin.dbserverManager.service.DbServerVO;
+import com.k4m.dx.tcontrol.db2pg.dbms.service.Db2pgSysInfVO;
 
 import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
 
 @Repository("DbmsDAO")
 public class DbmsDAO extends EgovAbstractMapper{
 
-	public List<Map<String, Object>> selectDbmsGrb() {
-		
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public List<Map<String, Object>> selectCharSetList(HashMap<String, Object> paramvalue) {
+			List<Map<String, Object>> sl = null;
+			sl = (List<Map<String, Object>>) list("dbmsSQL.selectCharSetList", paramvalue);		
+			return sl;
+		}
+
+	public int db2pg_sys_nmCheck(String db2pg_sys_nm) {
+		int resultSet = 0;
+		resultSet = (int) getSqlSession().selectOne("dbmsSQL.db2pg_sys_nmCheck", db2pg_sys_nm);
+		return resultSet;
+	}
+
+	public void insertDb2pgDBMS(Db2pgSysInfVO db2pgSysInfVO) {
+		insert("dbmsSQL.insertDb2pgDBMS", db2pgSysInfVO);		
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public List<Db2pgSysInfVO> selectDb2pgDBMS(Db2pgSysInfVO db2pgSysInfVO) {
+		List<Db2pgSysInfVO> sl = null;
+		sl = (List<Db2pgSysInfVO>) list("dbmsSQL.selectDb2pgDBMS", db2pgSysInfVO);
+		return sl;
+	}
+
+	public List<Map<String, Object>> dbmsGrb() {
 		List<Map<String, Object>> sl = null;
-		sl = (List<Map<String, Object>>) list("dbmsSQL.selectDbmsGrb", null);		
+		sl = (List<Map<String, Object>>) list("dbmsSQL.dbmsGrb", null);		
+		return sl;
+	}
+
+	public List<Map<String, Object>> dbmsListDbmsGrb() {
+		List<Map<String, Object>> sl = null;
+		sl = (List<Map<String, Object>>) list("dbmsSQL.dbmsListDbmsGrb", null);		
 		return sl;
 	}
 
--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/dbms/service/impl/DbmsServiceImpl.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/dbms/service/impl/DbmsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.k4m.dx.tcontrol.db2pg.dbms.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import com.k4m.dx.tcontrol.db2pg.dbms.service.Db2pgSysInfVO;
 import com.k4m.dx.tcontrol.db2pg.dbms.service.DbmsService;
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 
@@ -13,10 +16,38 @@ public class DbmsServiceImpl extends EgovAbstractServiceImpl implements DbmsServ
 
 	@Resource(name = "DbmsDAO")
 	private DbmsDAO dbmsDAO;
-	
+
 	@Override
-	public List<Map<String, Object>> selectDbmsGrb() throws Exception {
-		return dbmsDAO.selectDbmsGrb();
+	public List<Map<String, Object>> selectCharSetList(HashMap<String, Object> paramvalue) throws Exception {
+		return dbmsDAO.selectCharSetList(paramvalue);
 	}
+
+	@Override
+	public int db2pg_sys_nmCheck(String db2pg_sys_nm) throws Exception {
+		return dbmsDAO.db2pg_sys_nmCheck(db2pg_sys_nm);
+	}
+
+	@Override
+	public void insertDb2pgDBMS(Db2pgSysInfVO db2pgSysInfVO) throws Exception {
+		dbmsDAO.insertDb2pgDBMS(db2pgSysInfVO);
+		
+	}
+
+	@Override
+	public List<Db2pgSysInfVO> selectDb2pgDBMS(Db2pgSysInfVO db2pgSysInfVO) throws Exception {
+		return dbmsDAO.selectDb2pgDBMS(db2pgSysInfVO);		
+	}
+
+	@Override
+	public List<Map<String, Object>> dbmsGrb() throws Exception {
+		return dbmsDAO.dbmsGrb();		
+	}
+
+	@Override
+	public List<Map<String, Object>> dbmsListDbmsGrb() throws Exception {
+		return dbmsDAO.dbmsListDbmsGrb();	
+	}
+	
+
 
 }
