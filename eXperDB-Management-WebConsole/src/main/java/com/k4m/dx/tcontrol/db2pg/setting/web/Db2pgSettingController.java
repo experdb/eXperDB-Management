648aa6d7b3e5e408e27ef8d5ac5a96f0e36f6a13
joo01224 <kimjy@k4m.com>
2020-05-13 17:27:06 +0900

    [#noissue] type : bug body : updateDDLWork, updateDataWork query modified

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/setting/web/Db2pgSettingController.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/setting/web/Db2pgSettingController.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.sql.SQLException;
