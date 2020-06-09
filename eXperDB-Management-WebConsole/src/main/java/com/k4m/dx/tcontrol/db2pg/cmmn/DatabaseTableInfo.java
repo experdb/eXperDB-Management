4641065a6110c6c7477a77e77daeea0d354951eb
swbyu <swbyu@seungwoo-pc>
2020-06-09 17:13:15 +0900

    조건 % 옵션

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/cmmn/DatabaseTableInfo.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/cmmn/DatabaseTableInfo.java
@@ -43,7 +43,7 @@ public class DatabaseTableInfo {
 							+ "AND A.OBJECT_NAME NOT IN ('TOAD_PLAN_TABLE','PLAN_TABLE')  "
 							+ "AND A.OBJECT_NAME NOT LIKE 'MDRT%' "
 							+ "AND A.OBJECT_NAME NOT LIKE 'MDXT%' "		
-							+ "AND A.OBJECT_NAME Like '" +serverObj.get("TABLE_NM").toString().toUpperCase() + "'"
+							+ "AND A.OBJECT_NAME Like '%" +serverObj.get("TABLE_NM").toString().toUpperCase() + "%'"
 							+ "AND A.OBJECT_TYPE IN ('TABLE') ORDER BY 1 ";
 					
 					ResultSet oraRs = stmt.executeQuery(sql);				
@@ -69,7 +69,7 @@ public class DatabaseTableInfo {
 							+ "WHERE schema_id=(SELECT schema_id FROM sys.schemas WHERE name='"+serverObj.get("SCHEMA") +"') "													
 							+ "AND type in ('U') "
 							+ "AND type in ('U', 'V')"
-							+ "AND LOWER(name) Like '" + serverObj.get("TABLE_NM") + "'";
+							+ "AND LOWER(name) Like '%" + serverObj.get("TABLE_NM") + "%'";
 			
 					ResultSet msRs = stmt.executeQuery(sql);				
 					i = 0;
@@ -95,7 +95,7 @@ public class DatabaseTableInfo {
 				case "TC002204" :
 					sql = "SELECT n.nspname, c.relname, obj_description(c.oid) "
 							+ "FROM pg_catalog.pg_class c inner join pg_catalog.pg_namespace n on c.relnamespace=n.oid "
-							+ "WHERE c.relkind = 'r' and nspname ='"+serverObj.get("SCHEMA") +"' and c.relname LIKE '" + serverObj.get("TABLE_NM") + "' ORDER BY c.relname";
+							+ "WHERE c.relkind = 'r' and nspname ='"+serverObj.get("SCHEMA") +"' and c.relname LIKE '%" + serverObj.get("TABLE_NM") + "%' ORDER BY c.relname";
 
 					ResultSet rs = stmt.executeQuery(sql);				
 					i = 0;
@@ -118,7 +118,7 @@ public class DatabaseTableInfo {
 								+ "FROM SYSIBM.TABLES A, SYSIBM.SYSTABLES B "
 								+ "WHERE A.TABLE_SCHEMA = '" +serverObj.get("USER_ID").toString().toUpperCase() + "' "
 								+ "AND A.TABLE_NAME = B.NAME "
-								+ "AND A.TABLE_NAME LIKE '" +serverObj.get("TABLE_NM").toString().toUpperCase() + "' "
+								+ "AND A.TABLE_NAME LIKE '%" +serverObj.get("TABLE_NM").toString().toUpperCase() + "%' "
 								+ "AND A.TABLE_TYPE IN ('BASE TABLE') "
 								+ "AND A.TABLE_TYPE IN ('BASE TABLE','VIEW')";
 			
@@ -143,7 +143,7 @@ public class DatabaseTableInfo {
 									+ "FROM sysobjects "
 									+ "WHERE user_name(uid)='" +serverObj.get("USER_ID").toString().toUpperCase() + "' "
 									+ "AND type in ('U')"
-									+ "AND name LIKE'" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "' ";
+									+ "AND name LIKE '%" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "%' ";
 						
 						ResultSet syRs = stmt.executeQuery(sql);				
 						i = 0;
@@ -166,7 +166,7 @@ public class DatabaseTableInfo {
 						+ "FROM db_class "
 						+ "WHERE owner_name ='" +serverObj.get("USER_ID").toString().toUpperCase() + "'"
 						+ "AND is_system_class='NO' "
-						+ "AND class_name LIKE'" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "' "
+						+ "AND class_name LIKE '%" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "%' "
 						+ "AND class_type='CLASS' ORDER BY class_name ASC";
 
 					ResultSet cubRs = stmt.executeQuery(sql);				
@@ -191,7 +191,7 @@ public class DatabaseTableInfo {
 							+ "AND OBJECT_NAME NOT IN ('TOAD_PLAN_TABLE','PLAN_TABLE') "
 							+ "AND OBJECT_NAME NOT LIKE 'MDRT%'"
 							+ "AND OBJECT_NAME NOT LIKE 'MDXT%'"
-							+ "AND OBJECT_NAME LIKE'" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "' "
+							+ "AND OBJECT_NAME LIKE '%" +serverObj.get("TABLE_NM").toString().toUpperCase()  + "%' "
 							+ "AND OBJECT_TYPE IN ('TABLE')";
 					
 					ResultSet tibRs = stmt.executeQuery(sql);				
