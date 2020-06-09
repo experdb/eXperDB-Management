f5ea94c9c1514a4cc8520193d88eb1425042766d
joo01224 <kimjy@k4m.com>
2020-06-09 09:08:24 +0900

    [#noissue] type : feat body : version upgrade

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/WebConsoleSetting.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/WebConsoleSetting.java
@@ -15,7 +15,7 @@ public class WebConsoleSetting {
 	
 	public static void main(String[] args) throws Exception {
 		String strLanguage ="";
-		String strVersion ="eXperDB-Management-WebConsole-11.1";
+		String strVersion ="eXperDB-Management-WebConsole-11.1.2";
 		
 		String strDatabaseIp = "";
 		String strDatabasePort = "";
