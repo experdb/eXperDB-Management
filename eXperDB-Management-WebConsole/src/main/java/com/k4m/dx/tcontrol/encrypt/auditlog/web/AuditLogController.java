53b988fa9d68608e7fa4c9f5cc5be85466f9e1a2
swbyu <swbyu@seungwoo-pc>
2020-08-18 09:23:17 +0900

    암호화 디자인수정 java

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encrypt/auditlog/web/AuditLogController.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/encrypt/auditlog/web/AuditLogController.java
@@ -270,7 +270,9 @@ public class AuditLogController {
 	 */
 	@RequestMapping(value = "/popup/managementServerAuditLogDetail.do")
 	public ModelAndView managementServerAuditLogDetail(@ModelAttribute("historyVO") HistoryVO historyVO, HttpServletRequest request) {
-		ModelAndView mv = new ModelAndView();
+		
+		ModelAndView mv = new ModelAndView("jsonView");
+		
 		try {
 			// 화면접근이력 이력 남기기
 			CmmnUtils.saveHistory(request, historyVO);
@@ -292,7 +294,7 @@ public class AuditLogController {
 			mv.addObject("resultCode", resultCode);
 			mv.addObject("parameter", parameter);
 			mv.addObject("resultMessage", resultMessage);
-			mv.setViewName("encrypt/popup/managementServerAuditLogDetail");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
