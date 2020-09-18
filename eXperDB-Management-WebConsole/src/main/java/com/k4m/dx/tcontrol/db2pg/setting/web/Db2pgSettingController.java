c137f517bd6a2d3b83adb297f63b83735255fddc
seungwoo <swbyun@github.com>
2020-09-18 14:01:03 +0900

    import 수정

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/setting/web/Db2pgSettingController.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/db2pg/setting/web/Db2pgSettingController.java
@@ -22,7 +22,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.util.StringParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
