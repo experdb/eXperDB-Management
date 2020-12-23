fa03d99484dca0d07a6e74d344d69890040fe65a
seungwoo <swbyun@github.com>
2020-12-23 13:56:26 +0900

    version 11.2.6

--- eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/SHA256.java
+++ eXperDB-Management-WebConsole/src/main/java/com/k4m/dx/tcontrol/cmmn/SHA256.java
@@ -7,7 +7,6 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Base64;
 import java.util.Properties;
 import java.util.Random;
 
