d1b845bd93c00768b59501a489e94f0f9e0ef6e1
MJ-KIM0210 <greenmenthol1105@gmail.com>
2021-03-04 15:24:34 +0900

    ProxyAgentVO 생성

---
+++ eXperDB-Management-WebConsole/src/main/java/com/experdb/management/proxy/service/ProxyAgentVO.java
@@ -0,0 +1,107 @@
+package com.experdb.management.proxy.service;
+
+public class ProxyAgentVO {
+	
+	private int agt_sn; 
+	private String ipadr; 
+	private String domain_nm; 
+	private String root_pwd; 
+	private int socket_port; 
+	private String agt_cndt_cd; 
+	private String svr_use_yn; 
+	private String strt_dtm; 
+	private String istcnf_yn; 
+	private String agt_version; 
+	private String frst_regr_id; 
+	private String frst_reg_dtm; 
+	private String lst_mdfr_id; 
+	private String lst_mdf_dtm;
+	
+	public int getAgt_sn() {
+		return agt_sn;
+	}
+	public void setAgt_sn(int agt_sn) {
+		this.agt_sn = agt_sn;
+	}
+	public String getIpadr() {
+		return ipadr;
+	}
+	public void setIpadr(String ipadr) {
+		this.ipadr = ipadr;
+	}
+	public String getDomain_nm() {
+		return domain_nm;
+	}
+	public void setDomain_nm(String domain_nm) {
+		this.domain_nm = domain_nm;
+	}
+	public String getRoot_pwd() {
+		return root_pwd;
+	}
+	public void setRoot_pwd(String root_pwd) {
+		this.root_pwd = root_pwd;
+	}
+	public int getSocket_port() {
+		return socket_port;
+	}
+	public void setSocket_port(int socket_port) {
+		this.socket_port = socket_port;
+	}
+	public String getAgt_cndt_cd() {
+		return agt_cndt_cd;
+	}
+	public void setAgt_cndt_cd(String agt_cndt_cd) {
+		this.agt_cndt_cd = agt_cndt_cd;
+	}
+	public String getSvr_use_yn() {
+		return svr_use_yn;
+	}
+	public void setSvr_use_yn(String svr_use_yn) {
+		this.svr_use_yn = svr_use_yn;
+	}
+	public String getStrt_dtm() {
+		return strt_dtm;
+	}
+	public void setStrt_dtm(String strt_dtm) {
+		this.strt_dtm = strt_dtm;
+	}
+	public String getIstcnf_yn() {
+		return istcnf_yn;
+	}
+	public void setIstcnf_yn(String istcnf_yn) {
+		this.istcnf_yn = istcnf_yn;
+	}
+	public String getAgt_version() {
+		return agt_version;
+	}
+	public void setAgt_version(String agt_version) {
+		this.agt_version = agt_version;
+	}
+	public String getFrst_regr_id() {
+		return frst_regr_id;
+	}
+	public void setFrst_regr_id(String frst_regr_id) {
+		this.frst_regr_id = frst_regr_id;
+	}
+	public String getFrst_reg_dtm() {
+		return frst_reg_dtm;
+	}
+	public void setFrst_reg_dtm(String frst_reg_dtm) {
+		this.frst_reg_dtm = frst_reg_dtm;
+	}
+	public String getLst_mdfr_id() {
+		return lst_mdfr_id;
+	}
+	public void setLst_mdfr_id(String lst_mdfr_id) {
+		this.lst_mdfr_id = lst_mdfr_id;
+	}
+	public String getLst_mdf_dtm() {
+		return lst_mdf_dtm;
+	}
+	public void setLst_mdf_dtm(String lst_mdf_dtm) {
+		this.lst_mdf_dtm = lst_mdf_dtm;
+	}
+	
+	
+	
+}
