6a9f58dd9e04000c86a8377b9c10f5be39c827da
seungwoo <swbyun@github.com>
2021-04-20 13:16:52 +0900

    볼륨리스트 조회 추가

--- eXperDB-Management-WebConsole/src/main/java/com/experdb/management/backup/policy/web/ExperdbBackupPolicyController.java
+++ eXperDB-Management-WebConsole/src/main/java/com/experdb/management/backup/policy/web/ExperdbBackupPolicyController.java
@@ -14,12 +14,18 @@ import org.xml.sax.*;
 
 import com.experdb.management.backup.cmmn.*;
 import com.experdb.management.backup.policy.service.*;
+import com.k4m.dx.tcontrol.cmmn.client.ClientInfoCmmn;
+import com.k4m.dx.tcontrol.common.service.AgentInfoVO;
+import com.k4m.dx.tcontrol.common.service.CmmnServerInfoService;
 
 @Controller
 public class ExperdbBackupPolicyController {
 	
 	@Autowired
 	private ExperdbBackupPolicyService experdbBackupPolicyService;
+	
+	@Autowired
+	private CmmnServerInfoService cmmnServerInfoService;
 	
 	/**
 	 * 백업 정책을 등록한다
@@ -99,5 +105,37 @@ public class ExperdbBackupPolicyController {
 		return result;
     }
 	
+    
+    
+	/**
+	 * 볼륨리스트
+	 * 
+	 * @param request
+	 * @return JSONObject
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/experdb/getVolumes.do")
+	public @ResponseBody JSONObject getVolumes(HttpServletRequest request,@RequestParam Map<Object, String> param){
+		
+		JSONObject result = new JSONObject();
+		AgentInfoVO vo = new AgentInfoVO();
+		try {
+			
+			String ipadr = request.getParameter("ipadr");
+			
+			vo.setIPADR(ipadr);
+			
+			AgentInfoVO agentInfo =  (AgentInfoVO) cmmnServerInfoService.selectAgentInfo(vo);
+			String IP = ipadr;
+			int PORT = agentInfo.getSOCKET_PORT();
+
+			ClientInfoCmmn conn  = new ClientInfoCmmn();
+			result = conn.getVolumes( IP, PORT);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
