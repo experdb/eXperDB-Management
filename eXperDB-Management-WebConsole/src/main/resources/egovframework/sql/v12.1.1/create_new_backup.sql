e54f26b3c58ca58a89b6c68f788461ecedf34c31
seungwoo <swbyun@github.com>
2021-06-16 13:51:18 +0900

    restore create commit

---
+++ eXperDB-Management-WebConsole/src/main/resources/egovframework/sql/v12.1.1/create_new_backup.sql
@@ -0,0 +1,15 @@
+create table experdb_management.restoremachine(
+restoremachine_id     numeric not null default nextval('q_restore_machine_01'),
+mac varchar(512) NOT NULL,
+ip varchar(512) NOT NULL,
+subnetmask varchar(512) NOT NULL,
+gateway varchar(512) NOT NULL,
+dns varchar(512) NOT NULL,
+network varchar(512) NULL DEFAULT 'static',
+hostname varchar(512) NULL ,
+reboot varchar(512) NULL DEFAULT 'no',
+username varchar(512) NULL ,
+password  varchar(512) NULL);
+
+
+CREATE SEQUENCE Q_RESTORE_MACHINE_01; 
