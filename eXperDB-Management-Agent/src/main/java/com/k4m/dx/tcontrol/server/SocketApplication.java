11dcf58753951c2e719a99cd819394d24a9f68a7
MJ-KIM0210 <greenmenthol1105@gmail.com>
2022-01-10 10:49:00 +0900

    [#273] type : bug body : log4j 취약점 점검 이슈 처리 log4j -> log4j2 업데이트

--- eXperDB-Management-Agent/src/main/java/com/k4m/dx/tcontrol/server/SocketApplication.java
+++ eXperDB-Management-Agent/src/main/java/com/k4m/dx/tcontrol/server/SocketApplication.java
@@ -1,6 +1,8 @@
 package com.k4m.dx.tcontrol.server;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.json.simple.JSONObject;
 
 /**
@@ -17,7 +19,7 @@ import org.json.simple.JSONObject;
 */
 
 public interface SocketApplication {
-	public static Logger log = Logger.getLogger(SocketApplication.class);
+	public static Logger log = LogManager.getLogger(SocketApplication.class);
 	
 	public JSONObject perform(String tran_cd, JSONObject dbInfoObj) throws Exception;
 }
