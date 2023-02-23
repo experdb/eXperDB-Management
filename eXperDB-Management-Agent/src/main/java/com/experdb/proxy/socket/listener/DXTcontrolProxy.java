eef5cf78958a1b9a650783d809861561e9ba54dd
ksjj97inzent <ksjj97inzent@gmail.com>
2023-02-23 17:43:20 +0900

    프록시 소켓 리스너 추가

---
+++ eXperDB-Management-Agent/src/main/java/com/experdb/proxy/socket/listener/DXTcontrolProxy.java
@@ -0,0 +1,512 @@
+package com.experdb.proxy.socket.listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.jettison.json.JSONObject;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.experdb.proxy.db.repository.service.ProxyServiceImpl;
+import com.experdb.proxy.db.repository.service.SystemServiceImpl;
+import com.experdb.proxy.db.repository.vo.AgentInfoVO;
+import com.experdb.proxy.db.repository.vo.ProxyServerVO;
+import com.experdb.proxy.socket.SocketCtl;
+import com.experdb.proxy.util.FileUtil;
+import com.experdb.proxy.util.StrUtil;
+import com.k4m.dx.tcontrol.db.repository.vo.PryAgentInfoVO;
+
+/**
+ * 에이전트 실행 시 프록시 등록
+ * 
+* @author 
+* @see
+* 
+*      <pre>
+* == 개정이력(Modification Information) ==
+*
+*   수정일       수정자           수정내용
+*  -------     --------    ---------------------------
+*  미상		  	미상 			최초 생성
+*  2022.12		강병석		개정 이력 추가, 에이전트 통합
+*      </pre>
+*/
+
+public class DXTcontrolProxy extends SocketCtl {
+	private SchedulerFactory schedulerFactory = null;
+	private Scheduler scheduler = null;
+	private static ApplicationContext context;
+
+	private Logger socketLogger = LoggerFactory.getLogger("socketLogger");
+	private Logger errLogger = LoggerFactory.getLogger("errorLogger");
+	
+	public static void main(String[] args) {   
+		DXTcontrolProxy dXTcontrolProxy = new DXTcontrolProxy();
+		dXTcontrolProxy.start();
+	}
+
+	public DXTcontrolProxy() {
+		try {
+			schedulerFactory = new StdSchedulerFactory();
+			scheduler = schedulerFactory.getScheduler();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void start() {
+		String proxyServerChk = "";
+		
+		PryAgentInfoVO agtVo = new PryAgentInfoVO();
+
+		context = new ClassPathXmlApplicationContext(new String[] {"context-tcontrol.xml"});
+		ProxyServiceImpl service = (ProxyServiceImpl) context.getBean("ProxyService");
+		SystemServiceImpl systemService = (SystemServiceImpl) context.getBean("PrySystemService");
+		
+		try {
+			//선행조건  proxy 설치 확인
+			proxyServerChk = service.selectProxyServerChk("proxy_conf_which");	
+			String strIpadr = FileUtil.getPropertyValue("context.properties", "agent.install.ip");
+			String strPort = FileUtil.getPropertyValue("context.properties", "socket.server.port");
+			String strKeepalived = FileUtil.getPropertyValue("context.properties", "keepalived.install.yn");
+
+			agtVo.setIpadr(strIpadr);
+			PryAgentInfoVO agentInfoVO = systemService.selectPryAgtInfo(agtVo);
+			//proxy 설치시 테이블 insert 실행
+			//D의 경우 사용자 삭제로 자동등록 실행 안함
+			if (proxyServerChk != null && agentInfoVO != null) {
+				if (!"".equals(proxyServerChk) && ("".equals(agentInfoVO.getSvr_use_yn()) || !"D".equals(agentInfoVO.getSvr_use_yn()))) {
+					//proxy 서버일때 데이터 추가해야함
+					confSetProxyExecute(strIpadr, strPort, strKeepalived);
+				}
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * proxy 데이터 생성 및 등록
+	 * @param dbServerInfo
+	 * @throws Exception
+	 */
+	@Transactional
+	public String confSetProxyExecute(String strIpadr, String strPort, String strKeepalived) throws Exception {
+		ProxyServerVO searchProxyServerVO = new ProxyServerVO();
+		searchProxyServerVO.setIpadr(strIpadr);
+
+		Map<String, Object> param = new HashMap<String, Object>();
+		ProxyServerVO vo = new ProxyServerVO();
+		String returnMsg = "";
+
+		ProxyServiceImpl pryService = (ProxyServiceImpl) context.getBean("ProxyService");
+		SystemServiceImpl systemService = (SystemServiceImpl) context.getBean("PrySystemService");
+
+		Map<String, Object> insertParam = new HashMap<String, Object>();
+		
+		try {
+			String proxyPathData = "";
+			String KeepPathData = "";
+			int dbSvrIdData = 0;
+			int peerIdData = 0;
+			String dbSvrNmData = "";
+			String peerIpData = "";
+			String back_peerIpData = "";
+			String masterGbnData = "";
+			String old_masterGbnData = "";
+			String upd_masterGbnData = "";
+			String proxyExeStaus = "";		//proxy 상태
+			String keepExeStaus = "";		//keepalived 상태
+			String prySvrUseYn = "N";		//pry_svr_사용여부
+			String proxySvrNmData = "";		//pry svr 명
+
+			String insUpNmGbn = "";
+
+			String strMaxConn = "";
+			String strTimeClient = "";
+			String strTimeConnect = "";
+			String strTimeServer = "";
+			String strTimeCheck = "";
+			String stateMasterInterface = "";
+			String strObjIp = "";
+			String strPeerServerIp = "";
+
+			String strlisnerList="";
+			String strlisnerSvrList="";
+			String strVipConfList="";
+			String strKeepInstallYn = "";
+
+			JSONObject jObjResult = null;
+			JSONObject jObjKeepResult = null;
+			socketLogger.info("ProxyServiceImpl.searchProxyServerVO : " + strIpadr);
+			//proxy 서버 등록 여부 확인
+			ProxyServerVO proxyServerInfo = pryService.selectPrySvrInfo(searchProxyServerVO);
+			//socketLogger.info("ProxyServiceImpl.proxyServerInfo : " + proxyServerInfo.toString());
+			//keepalived 설치 상태
+			strKeepInstallYn = strKeepalived;
+
+			//1. proxy conf 위치
+			proxyPathData = pryService.selectProxyServerChk("proxy_conf_which"); //param setting
+
+			//2. keepalived conf 위치
+			KeepPathData = pryService.selectProxyServerChk("keep_conf_which"); //param setting
+
+			//proxy conf 존재일 경우 만 등록
+			if (proxyPathData != null) {
+				jObjResult = pryService.selectProxyServerList("proxy_conf_read", proxyPathData, strIpadr,""); //proxy conf setting
+
+				if (jObjResult.length() > 0) {
+					dbSvrIdData = ((Integer)jObjResult.get("db_svr_id")).intValue();
+					dbSvrNmData = (String)jObjResult.get("db_svr_nm");
+
+					//global
+					strMaxConn = (String)jObjResult.get("max_conn");
+					strTimeClient = (String)jObjResult.get("time_client");
+					strTimeConnect = (String)jObjResult.get("time_connect");
+					strTimeServer = (String)jObjResult.get("time_server");
+					strTimeCheck = (String)jObjResult.get("time_check");
+
+					//list
+					strlisnerList = (String)jObjResult.get("lisner_list");
+					strlisnerSvrList = (String)jObjResult.get("lisner_svr_list");
+
+				} else {
+					dbSvrIdData = 0;
+					dbSvrNmData = "";
+
+					//global
+					strMaxConn = "";
+					strTimeClient = "";
+					strTimeConnect = "";
+					strTimeServer = "";
+					strTimeCheck = "";
+
+					strlisnerList = "";
+					strlisnerSvrList = "";
+				}
+			}
+			/////////////////////////////////////////////////////////////////////
+
+			//4. keepalived conf 열기
+			if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+				jObjKeepResult = pryService.selectProxyServerList("keepalived_conf_read", KeepPathData, strIpadr,""); //param setting
+				socketLogger.info("#ProxyServiceImpl.jObjKeepResult : " + jObjKeepResult);
+				if (jObjKeepResult.length() > 0) {
+					peerIpData = (String)jObjKeepResult.get("peer_id");
+					masterGbnData = (String)jObjKeepResult.get("master_gbn");
+
+					stateMasterInterface = (String)jObjKeepResult.get("if_nm");
+					strObjIp = (String)jObjKeepResult.get("obj_ip");
+					strPeerServerIp = (String)jObjKeepResult.get("peer_server_ip");
+
+					strVipConfList = (String)jObjKeepResult.get("vip_conf_list");
+
+					back_peerIpData = (String)jObjKeepResult.get("back_peer_id");
+					
+				} else {
+					peerIpData = "";
+					masterGbnData = "M";
+
+					stateMasterInterface = "";
+					strObjIp = "";
+					strPeerServerIp = "";
+
+					strVipConfList = "";
+					
+					back_peerIpData = "";
+				}
+			} else {
+				peerIpData = "";
+				masterGbnData = "M"; //무조건 마스터가 됨
+
+				stateMasterInterface = "";
+				strObjIp = ""; //
+				strPeerServerIp = "";
+
+				strVipConfList = "";
+				
+				back_peerIpData = "";
+			}
+			/////////////////////////////////////////////////////////////////////
+			socketLogger.info("ProxyServiceImpl.masterGbnDatamasterGbnData : " + masterGbnData);
+			//5. proxy 실행상태
+			if (proxyPathData != null) {
+				proxyExeStaus = pryService.selectProxyServerChk("proxy_setting_tot"); //param setting
+				if (proxyExeStaus != null) {
+					proxyExeStaus = proxyExeStaus.trim();
+					if ("running".equals(proxyExeStaus)) {
+						proxyExeStaus = "TC001501";
+					} else {
+						proxyExeStaus = "TC001502";
+					}
+				} else {
+					proxyExeStaus = "TC001502";
+				}
+			}
+			/////////////////////////////////////////////////////////////////////
+
+			//6. keepalived 실행상태
+			if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+				keepExeStaus = pryService.selectProxyServerChk("keepalived_setting_tot"); //param setting
+				if (keepExeStaus != null) {
+					keepExeStaus = keepExeStaus.trim();
+					if ("running".equals(keepExeStaus)) {
+						keepExeStaus = "TC001501";
+					} else {
+						keepExeStaus = "TC001502";
+					}
+				} else {
+					keepExeStaus = "TC001502";
+				}
+
+				//사용여부
+				if ("TC001501".equals(keepExeStaus) && "TC001501".equals(keepExeStaus)) {
+					prySvrUseYn = "Y";
+				} else {
+					prySvrUseYn = "N";
+				}
+			} else {
+				keepExeStaus = "";
+				prySvrUseYn = "";
+			}
+			/////////////////////////////////////////////////////////////////////
+
+			////////////////////////////////////PRY_SVR_NM 설정//////////////////////////////
+			if (masterGbnData != null) {
+				if ("M".equals(masterGbnData)) {
+					if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+						proxySvrNmData = dbSvrNmData + "_proxy_1";
+					} else {
+						searchProxyServerVO.setDb_svr_id(((Integer)jObjResult.get("db_svr_id")).intValue());
+						
+						ProxyServerVO proxyServerVOBack = systemService.selectDBMSSvrMaxNmInfo(searchProxyServerVO);
+						
+						if (proxyServerVOBack == null) {
+							proxySvrNmData = dbSvrNmData + "_proxy_1";
+						} else {
+							String masterSvrNm = "";
+							String masterSvrFirst = "";
+							int iMasterSvrEnd = 0;
+							
+							ProxyServerVO masterProxyServerInfo = pryService.selectPrySvrInfo(searchProxyServerVO);
+
+							if (masterProxyServerInfo != null && masterProxyServerInfo.getPry_svr_nm() != null && !"".equals(masterProxyServerInfo.getPry_svr_nm())) {
+								proxySvrNmData = masterProxyServerInfo.getPry_svr_nm();
+							} else {
+								if (proxyServerVOBack.getPry_svr_nm() != null && !"".equals(proxyServerVOBack.getPry_svr_nm())) {
+									masterSvrNm = proxyServerVOBack.getPry_svr_nm();
+
+									masterSvrFirst = masterSvrNm.substring(0 , masterSvrNm.lastIndexOf("_")+1);
+									iMasterSvrEnd = Integer.parseInt((masterSvrNm.substring(masterSvrNm.lastIndexOf("_") + 1 , masterSvrNm.length()))) + 1;
+									proxySvrNmData = masterSvrFirst + Integer.toString(iMasterSvrEnd);
+								} else {
+									proxySvrNmData = dbSvrNmData + "_proxy_1";
+								}
+							}
+						}	
+					}
+				} else {
+					ProxyServerVO proxyServerVOBack = systemService.selectPrySvrMaxNmInfo(searchProxyServerVO);
+
+					if (proxyServerVOBack == null) {
+						proxySvrNmData = dbSvrNmData + "_proxy_2";
+					} else {
+						String masterSvrNm = "";
+						String masterSvrFirst = "";
+						int iMasterSvrEnd = 0;
+						
+						ProxyServerVO masterProxyServerInfo = pryService.selectPrySvrInfo(searchProxyServerVO);
+
+						if (masterProxyServerInfo != null && masterProxyServerInfo.getPry_svr_nm() != null && !"".equals(masterProxyServerInfo.getPry_svr_nm())) {
+							proxySvrNmData = masterProxyServerInfo.getPry_svr_nm();
+						} else {
+							if (proxyServerVOBack.getPry_svr_nm() != null && !"".equals(proxyServerVOBack.getPry_svr_nm())) {
+								masterSvrNm = proxyServerVOBack.getPry_svr_nm();
+
+								masterSvrFirst = masterSvrNm.substring(0 , masterSvrNm.lastIndexOf("_")+1);
+								iMasterSvrEnd = Integer.parseInt((masterSvrNm.substring(masterSvrNm.lastIndexOf("_") + 1 , masterSvrNm.length()))) + 1;
+								proxySvrNmData = masterSvrFirst + Integer.toString(iMasterSvrEnd);
+							} else {
+								proxySvrNmData = dbSvrNmData + "_proxy_2";
+							}
+						}
+					}
+				}
+				
+				
+			}
+
+			//master svr id
+			if (peerIpData != null && !peerIpData.equals("")) {
+				searchProxyServerVO.setIpadr(peerIpData);
+
+				ProxyServerVO masterProxyServerInfo = pryService.selectPrySvrInslInfo(searchProxyServerVO);
+				if (masterProxyServerInfo != null) {
+					peerIdData = masterProxyServerInfo.getPry_svr_id();
+				}
+			}
+
+			//proxy 서버 저장
+			vo.setIpadr(strIpadr);
+			vo.setPry_pth(proxyPathData);
+			
+			if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+				vo.setKal_install_yn("Y");
+				vo.setKal_pth(KeepPathData);
+				
+			} else {
+				vo.setKal_install_yn("N");
+				vo.setKal_pth(null);
+			}
+
+			vo.setDb_svr_id(dbSvrIdData);
+			
+			vo.setFrst_regr_id("system");
+			vo.setLst_mdfr_id("system");
+
+			vo.setExe_status(proxyExeStaus);
+			vo.setKal_exe_status(keepExeStaus);
+			vo.setUse_yn(prySvrUseYn);
+			
+			searchProxyServerVO.setDb_svr_id(((Integer)jObjResult.get("db_svr_id")).intValue());
+			searchProxyServerVO.setIpadr(strIpadr);
+		
+			ProxyServerVO proxyOldServerInfo = systemService.selectDBMSSvrEtcMaxNmInfo(searchProxyServerVO);
+
+			//old_master_gbn setting
+			if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+				old_masterGbnData = masterGbnData;
+			} else {
+				if (proxyOldServerInfo != null) {
+					if (proxyOldServerInfo.getPry_svr_nm() != null && !"".equals(proxyOldServerInfo.getPry_svr_nm())) {
+						old_masterGbnData = "S";
+					} else {
+						old_masterGbnData = "M";
+					}
+				} else {
+					old_masterGbnData = "M";
+				}
+			}
+			
+			if (proxyOldServerInfo == null) {
+				upd_masterGbnData = "M";
+			} else {
+				upd_masterGbnData = "S";
+			}
+
+			//null 이면 insert(t_pry_svr_i)
+			if(proxyServerInfo == null) {
+				vo.setDay_data_del_term(30);
+				vo.setMin_data_del_term(3);
+				vo.setPry_svr_nm(proxySvrNmData);
+				vo.setMaster_gbn(masterGbnData);
+				vo.setOld_master_gbn(old_masterGbnData);
+
+				if (peerIdData != 0) {
+					vo.setMaster_svr_id_chk(Integer.toString(peerIdData));
+				} else {
+					vo.setMaster_svr_id_chk(null);
+				}
+				
+				vo.setBack_peer_id(back_peerIpData);
+
+				insUpNmGbn = "proxySvrIns";
+			} else { //null 이 아니면 false
+				vo.setPry_svr_id(proxyServerInfo.getPry_svr_id()); //proxy server id
+				
+				if (vo.getPry_pth() == null) {
+					vo.setPry_pth(proxyServerInfo.getPry_pth());
+				}
+	
+				if (vo.getKal_pth() == null) {
+					vo.setKal_pth(proxyServerInfo.getKal_pth());
+				}
+
+				vo.setAgt_sn(proxyServerInfo.getAgt_sn());
+				vo.setDay_data_del_term(StrUtil.nvlIntChg(proxyServerInfo.getDay_data_del_term(),30));
+				vo.setMin_data_del_term(StrUtil.nvlIntChg(proxyServerInfo.getMin_data_del_term(),3));
+				vo.setPry_svr_nm(proxySvrNmData);
+
+				vo.setMaster_gbn(masterGbnData);
+				vo.setOld_master_gbn(old_masterGbnData);
+				vo.setUpd_master_gbn(upd_masterGbnData);
+				
+			//	vo.setMaster_gbn(proxyServerInfo.getMaster_gbn());
+				Integer master_svr_id_num = proxyServerInfo.getMaster_svr_id();
+				socketLogger.info("strKeepInstallYn : " + master_svr_id_num);
+				if (strKeepInstallYn != null && "Y".equals(strKeepInstallYn)) {
+					socketLogger.info("master_svr_id_nummaster_svr_id_num : " + master_svr_id_num);
+					if (master_svr_id_num != null) {
+						if (master_svr_id_num > 0) {
+							vo.setMaster_svr_id_chk( Integer.toString(proxyServerInfo.getMaster_svr_id()));
+						} else {
+							if (peerIdData != 0) {
+								vo.setMaster_svr_id_chk("Y");
+							} else {
+								vo.setMaster_svr_id_chk(null);
+							}
+						}
+					} else {
+						if (peerIdData != 0) {
+							vo.setMaster_svr_id_chk("Y");
+						} else {
+							vo.setMaster_svr_id_chk(null);
+						}
+					}
+				} else {
+					vo.setMaster_svr_id_chk(null);
+				}
+				socketLogger.info("peerIdData : " + peerIdData);
+				socketLogger.info("vo.getMaster_svr_id_chkgetMaster_svr_id_chkgetMaster_svr_id_chk() : " + vo.getMaster_svr_id_chk());
+				vo.setBack_peer_id(null);
+
+				insUpNmGbn = "proxySvrUdt";
+			}
+			//////////////////////////////////////////////////////////////////
+
+			//global setting
+			if (strMaxConn != null) {
+				insertParam.put("max_conn", strMaxConn);
+				insertParam.put("time_client", strTimeClient);
+				insertParam.put("time_connect", strTimeConnect);
+				insertParam.put("time_server", strTimeServer);
+				insertParam.put("time_check", strTimeCheck);
+				insertParam.put("if_nm", stateMasterInterface);
+				insertParam.put("obj_ip", strObjIp);
+				insertParam.put("peer_server_ip", strPeerServerIp);
+
+				insertParam.put("lisner_list", strlisnerList);
+				insertParam.put("lisner_svr_list", strlisnerSvrList);
+				insertParam.put("vip_conf_list", strVipConfList);
+			} else {
+				insertParam.put("max_conn", "");
+				insertParam.put("time_client", "");
+				insertParam.put("time_connect", "");
+				insertParam.put("time_server", "");
+				insertParam.put("time_check", "");
+				insertParam.put("if_nm", "");
+				insertParam.put("obj_ip", "");
+				insertParam.put("peer_server_ip", "");
+				
+				insertParam.put("lisner_list", "");
+				insertParam.put("lisner_svr_list", "");
+				insertParam.put("vip_conf_list", "");
+			}
+
+			if (proxyPathData != null) {
+				returnMsg = pryService.proxyConfFisrtIns(vo, insUpNmGbn, insertParam);
+			}
+		} catch (Exception e) {
+			errLogger.error("DXTcontrolProxy {} ", e.toString());
+			returnMsg = "false";
+		}
+		return returnMsg;
+	}
+}
---
+++ eXperDB-Management-Agent/src/main/java/com/experdb/proxy/socket/listener/DXTcontrolSocketExecute.java
@@ -0,0 +1,180 @@
+package com.experdb.proxy.socket.listener;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.experdb.proxy.server.PsP001;
+import com.experdb.proxy.server.PsP002;
+import com.experdb.proxy.server.PsP003;
+import com.experdb.proxy.server.PsP004;
+import com.experdb.proxy.server.PsP005;
+import com.experdb.proxy.server.PsP006;
+import com.experdb.proxy.server.PsP007;
+import com.experdb.proxy.server.PsP008;
+import com.experdb.proxy.server.PsP009;
+import com.experdb.proxy.server.PsP010;
+import com.experdb.proxy.server.PsP011;
+import com.experdb.proxy.server.PsP012;
+import com.experdb.proxy.server.PsP013;
+import com.experdb.proxy.server.PsP014;
+import com.experdb.proxy.socket.ProtocolID;
+import com.experdb.proxy.socket.SocketCtl;
+import com.experdb.proxy.socket.TranCodeType;
+
+/**
+ * com.experdb.proxy.server 기능 로드 밸런스
+ * 
+* @author 최정환
+* @see
+* 
+*      <pre>
+* == 개정이력(Modification Information) ==
+*
+*   수정일       수정자           수정내용
+*  -------     --------    ---------------------------
+*  2021.02.24   최정환 	최초 생성
+*  2022.12.12	강병석		에이전트 통합, 미사용 기능 비활성(5, 11, 13)
+*      </pre>
+*/
+public class DXTcontrolSocketExecute extends SocketCtl implements Runnable {
+	
+	private int			localport = 0;
+	
+	private Logger errLogger = LoggerFactory.getLogger("errorLogger");
+	private Logger socketLogger = LoggerFactory.getLogger("socketLogger");
+	
+	public DXTcontrolSocketExecute(Socket socket) {
+		client = socket;
+		localport = client.getLocalPort();
+	}
+	
+	public void run() {
+		try {
+			is = new BufferedInputStream(client.getInputStream());
+			os = new BufferedOutputStream(client.getOutputStream());
+			
+			//while(true) {
+				byte[] recvBuff = recv(TotalLengthBit, false);
+				
+				JSONParser parser=new JSONParser();
+				Object obj=parser.parse(new String(recvBuff));
+				
+				recvBuff = null;
+				
+				JSONObject jObj = (JSONObject) obj;
+				//JSONArray jArray=(JSONArray) jObj.get(ProtocolID._tran_req_data);
+					
+				String strDX_EX_CODE = (String) jObj.get(ProtocolID.DX_EX_CODE);
+				
+				JSONObject objSERVER_INFO = new JSONObject(); 
+
+				switch(strDX_EX_CODE) {
+				//Database List
+				case TranCodeType.PsP001 : //proxy 에이전트 setting
+					
+					PsP001 psP001 = new PsP001(client, is, os);
+					psP001.execute(strDX_EX_CODE, jObj);
+		
+					break;
+				case TranCodeType.PsP002 ://proxy 에이전트 연결 Test
+					
+					PsP002 psP002 = new PsP002(client, is, os);
+					psP002.execute(strDX_EX_CODE, jObj);
+		
+					break;
+				case TranCodeType.PsP003 :// get config file
+					
+					PsP003 psP003 = new PsP003(client, is, os);
+					psP003.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP004 : //proxy conf 파일 백업 & 신규 생성 
+					
+					PsP004 psP004 = new PsP004(client, is, os);
+					psP004.execute(strDX_EX_CODE, jObj);
+		
+					break;
+				case TranCodeType.PsP005 : //proxy service restart
+					
+//					PsP005 psP005 = new PsP005(client, is, os);
+//					psP005.execute(strDX_EX_CODE, jObj);
+		
+					break;
+				case TranCodeType.PsP006 : //proxy service start/stop
+					
+					PsP006 PsP006 = new PsP006(client, is, os);
+					PsP006.execute(strDX_EX_CODE, jObj);
+		
+					break;
+				case TranCodeType.PsP007 : //get network Interface search
+					
+					PsP007 PsP007 = new PsP007(client, is, os);
+					PsP007.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP008 : // get log file search
+					
+					PsP008 PsP008 = new PsP008(client, is, os);
+					PsP008.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP009 : // proxy conf 파일 searh 후 데이터 입력 요청
+					
+					PsP009 PsP009 = new PsP009(client, is, os);
+					PsP009.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP010 : // check installed keepalived
+					
+					PsP010 PsP010 = new PsP010(client, is, os);
+					PsP010.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP011 : // 설정 백업 폴더 삭제
+					
+//					PsP011 PsP011 = new PsP011(client, is, os);
+//					PsP011.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP012 : // check proxy server
+					
+					PsP012 PsP012 = new PsP012(client, is, os);
+					PsP012.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP013 : // delete backup conf File
+					
+//					PsP013 PsP013 = new PsP013(client, is, os);
+//					PsP013.execute(strDX_EX_CODE, jObj);
+					
+					break;
+				case TranCodeType.PsP014 : // Scale In
+					
+					PsP014 PsP014 = new PsP014(client, is, os);
+					PsP014.execute(strDX_EX_CODE, jObj);
+					
+					break;
+					
+				}
+				objSERVER_INFO = null;
+
+			//}
+			
+		} catch(Exception e) {
+			errLogger.error("{} {}", "experDB Socket Execute Error : ", e.toString());
+		}finally{
+			try{
+				client.close();
+			}catch(IOException e){
+				errLogger.error("{} {}", "experDB Socket Close Error : ", e.toString());
+			}
+		}
+	}
+}
