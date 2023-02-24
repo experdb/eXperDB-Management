d0adc3933d7e74ef0be7d81ddd85544beb985c35
ksjj97inzent <ksjj97inzent@gmail.com>
2023-02-24 14:36:57 +0900

    프록시 로그 삭제 실행 파일 위치 변경

--- eXperDB-Management-Agent/bin/proxy_logFile_delete.sh
+++ eXperDB-Management-Agent/bin/proxy_logFile_delete.sh
@@ -10,7 +10,7 @@ PROXY_DIR=/var/log/haproxy
 KEEPALIVED_DIR=/var/log/keepalived
 PROXY_DIR_SS=/var/log/haproxy/detail
 
-PROXY_conf_DIR=/root/app/eXperDB-Proxy-Agent/backup
+PROXY_conf_DIR=/experdb/app/eXperDB-Management/eXperDB-Management-Agent/backup
 
 NOW_DATE=$(date "+%Y/%m/%d %T")
 
